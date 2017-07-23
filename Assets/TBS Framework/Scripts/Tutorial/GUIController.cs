252556946136adc0fc8cb64ed9854737fb13f28c
David Cummins <davepyronez@gmail.com>
2017-07-23 22:45:18 +0100

    initial commit

---
+++ Assets/TBS Framework/Scripts/Tutorial/GUIController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class GUIController : MonoBehaviour
+{
+    public CellGrid CellGrid;
+
+	void Update ()
+    {
+        if(Input.GetKeyDown(KeyCode.N))
+        {
+            CellGrid.EndTurn();//User ends his turn by pressing "n" on keyboard.
+        }
+	}
+}
